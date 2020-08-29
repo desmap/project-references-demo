43e4e490c9af3c1c844c851cd2b146ccf758c2bf
desmap <desmondmappa@gmail.com>
2020-08-29 06:33:09 +0200

    update

---
+++ another-package/index.ts
@@ -0,0 +1,8 @@
+import { createZoo } from '../zoo/zoo'
+import { Dog } from '../animals/dog'
+
+const newZoo: Dog[] = createZoo()
+
+// console.log(newZoo[0].deliberateError);
+console.log(49)
+console.log(newZoo)
--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,35 @@
-const path = require('path');
-const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+const path = require('path')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
 
 module.exports = {
-  "mode": "development",
-  "entry": "src/index.ts",
-  "output": {
-      "path": __dirname+'/dist',
-      "filename": "[name].js"
+  mode: 'development',
+  entry: 'another-package',
+  output: {
+    path: path.resolve('dist/another-package'),
   },
-  "watch": false,
-  "context": __dirname, // to automatically find tsconfig.json
-  "module": {
-      "rules": [
-          {
-              "test": /\.tsx?$/,
-              "exclude": /node_modules/,
-              "use": {
-                  "loader": "babel-loader",
-                  options: {
-                    rootMode: 'upward',
-                  },
-                  // "loader": "ts-loader",
-                  // "options": {
-                  //     "transpileOnly": false,
-                  //     "projectReferences": false
-                  // }
-              }
-          }
-      ]
+  watch: false,
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            rootMode: 'upward',
+          },
+          // "loader": "ts-loader",
+          // "options": {
+          //     "transpileOnly": false,
+          //     "projectReferences": false
+          // }
+        },
+      },
+    ],
   },
   resolve: {
-    modules: [
-      "node_modules",
-      path.resolve(__dirname)
-    ],
-    extensions: [".js", ".ts"]
+    modules: ['node_modules', path.resolve(__dirname)],
+    extensions: ['.js', '.ts'],
   },
   // plugins: [new ForkTsCheckerWebpackPlugin()]
 }
